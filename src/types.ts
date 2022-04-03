1da7f100a560f0b114d3ff37c164cd50b784b445
Valery Zinchenko <minicablestone@gmail.com>
2022-04-03 20:52:18 +0300

    add more details to types

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 export type Primitive = "string" | "integer" | "number" | "boolean" | "array" | "object"
 export type Schema =
-  | { type: Exclude<Primitive, "array" | "object"> }
+  | { type: Exclude<Primitive, "array" | "object">, default?: Primitive, enum?: string[] }
   | { type: "array", items: Schema }
   | { type: "object", $ref?: string, required?: string[], properties?: Record<string, Schema> }
   | { type: undefined, $ref: string }
@@ -8,13 +8,14 @@ export type Schema =
 export interface Parameter {
   name: string
   in: "path" | "query"
-  required: boolean
-  style: "simple" | "form"
-  explode: boolean
+  required?: boolean
+  style?: "simple" | "form"
+  explode?: boolean
   schema: Schema
 }
 
 export type PathMethod = Record<string, {
+  description?: string
   parameters?: Parameter[]
   requestBody?: {
     content: {
