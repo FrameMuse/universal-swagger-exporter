d9296d43c49eafaba4535297c9a3db6fdcab4e41
Valery Zinchenko <minicablestone@gmail.com>
2022-05-27 15:52:38 +0300

    Schema type revamp add JSDocs add other fields

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,74 @@
 export type Primitive = "string" | "integer" | "number" | "boolean" | "array" | "object"
-export type Schema =
-  | { type: Exclude<Primitive, "array" | "object">, default?: Primitive, enum?: string[] }
-  | { type: "array", items: Schema }
-  | { type: "object", $ref?: string, required?: string[], properties?: Record<string, Schema> }
-  | { type: undefined, $ref: string }
+// export type Schema = (
+//   | { type: Exclude<Primitive, "array" | "object">, default?: Primitive }
+//   | { type: "array", items: Schema }
+//   | { type: "object", $ref?: string, required?: string[], properties?: Record<string, Schema> }
+//   | { type: undefined, $ref: string }) & { nullable?: boolean, allOf?: Schema[], enum?: string[] }
+
+interface SchemaArray {
+  type: "array"
+  /**
+   * Items that are represented by this `array`.
+   */
+  items?: Schema
+  /**
+   * Means that the value of the `Schema` may be `null`.
+   */
+  nullable?: boolean
+}
+
+interface SchemaObject {
+  type: "object"
+  properties?: Record<string, Schema>
+  /**
+   * List of required fields related to `properties` field.
+   * @example ["field1", "field2"]
+   */
+  required?: string[]
+  /**
+   * Means that the value of the `Schema` may be `null`.
+   */
+  nullable?: boolean
+}
+
+interface SchemaAny {
+  /**
+   * Type of the `Schema`.
+   */
+  type?: Exclude<Primitive, "array" | "object">
+  /**
+   * Default value of the `Schema`.
+   */
+  default?: Primitive
+  /**
+   * Reference to another `Schema`.
+   * 
+   * @example "#/components/schemas/SchemaName"
+   */
+  $ref?: string
+  /**
+   * Means that the value of the `Schema` may be `null`.
+   */
+  nullable?: boolean
+  /**
+   * Represents union of possible `string` values.
+   * It means that the `Schema` has `string` type.
+   * The unions are joined by `|` sign.
+   * 
+   * @example "guest" | "user"
+   */
+  enum?: string[]
+  /**
+   * Represents union of possible values.
+   * The unions are joined by `&` sign.
+   * 
+   * @example { foo: string } & { bar: number[] }
+   * @example SchemaName1 & SchemaName2
+   */
+  allOf?: Schema[]
+}
+
+export type Schema = SchemaArray | SchemaObject | SchemaAny
 
 export interface Parameter {
   name: string
@@ -12,6 +77,7 @@ export interface Parameter {
   style?: "simple" | "form"
   explode?: boolean
   schema: Schema
+  description?: string
 }
 
 export type PathMethod = Record<string, {
@@ -19,19 +85,18 @@ export type PathMethod = Record<string, {
   parameters?: Parameter[]
   requestBody?: {
     content: {
-      "multipart/form-data": { schema: Schema }
+      [k in string]: { schema: Schema }
     }
   }
   responses: Record<string, {
-    content: {
-      "application/json": {
-        schema: Schema
-      }
+    content?: {
+      [k in string]: { schema: Schema }
     }
+    description?: string
   }>
 }>
 
 export type Paths = Record<string, PathMethod>
 export type Schemas = Record<string, Schema>
 
-export type ActionArgs = Record<string, { required: boolean, schemaType: string }>
+export type ActionArgs = Record<string, Omit<Parameter, "required" | "schema"> & { required: boolean, schemaType: string }>
